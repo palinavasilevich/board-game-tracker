ed4aa0a29531ab52b0c3b2d3f5ffe8ef3767af1c
pvslvch <polina.vasilevichh@gmail.com>
2026-05-13 10:29:05 +0200

    feat(ui): add Typography components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { TypographyH1 } from "@/components/ui/typography";
+
 export default function Home() {
   return (
     <div>
-      <main>Board Game Tracker</main>
+      <main>
+        <TypographyH1>Board Game Tracker</TypographyH1>
+      </main>
     </div>
   );
 }
---
+++ components/ui/typography.tsx
@@ -0,0 +1,15 @@
+export function TypographyH1({ children }: { children: React.ReactNode }) {
+  return (
+    <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+      {children}
+    </h1>
+  );
+}
+
+export function TypographyH3({ children }: { children: React.ReactNode }) {
+  return (
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      {children}
+    </h3>
+  );
+}
